4f36c5f4bf0ba3df85a89f8cfb695feddf80b55e
Mathoho, K, Miss [24774898@sun.ac.za] <24774898@sun.ac.za>
2023-07-28 12:45:14 +0200

    [#16]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * @author Mathoho Konanani
+ */
 public class Greeter {
 
+    /**
+     * prints a a greeting in the terminal
+     */
     public static void greeter(){
         System.out.print("hello ");
     }
 
+    /**
+     * it gets the name from the user input
+     * @return the name of the person
+     */
     public static String getName(){
         Scanner scan = new Scanner(System.in);
         String nme = scan.nextLine();
         scan.close();
         return nme;
     } 
+    /**
+     * the main method
+     * @param args the array of commandline arguments
+     */
     public static void main(String[] args){
         String name = getName();
         greeter();
